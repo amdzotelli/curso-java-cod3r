dd78958bd2031c970ad8547a7a89e646cdf94931
Amanda Zotelli <amanda.zotelli@mercadolivre.com>
2022-10-12 19:42:45 -0300

    Cria o segundo programa do curso, aula sobre tipagem

---
+++ exercicios/programa01/src/fundamentos/AreaCircunferencia.java
@@ -0,0 +1,18 @@
+package fundamentos;
+
+import static java.lang.System.*;
+
+public class AreaCircunferencia {
+
+    public static void main(String[] args) {
+
+        double raio = 3.3;
+        final double PI = 3.14159;
+
+        double area = PI * raio * raio;
+
+        out.println(area);
+
+    }
+
+}
