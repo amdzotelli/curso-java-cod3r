86470934a062898e7349e422436e521b85143866
Amanda Zotelli <amanda.zotelli@mercadolivre.com>
2022-10-12 19:41:06 -0300

    Cria o primeiro programa do curso

---
+++ exercicios/programa01/src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+import static java.lang.System.*;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+
+        out.println("Hello world!");
+        out.println("Hello world 02!");
+
+    }
+
+}
