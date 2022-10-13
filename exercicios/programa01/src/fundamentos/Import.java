7fe94e836fba56a1e7c3b7bcbfc917eaf0ea218f
Amanda Zotelli <amanda.zotelli@mercadolivre.com>
2022-10-13 20:18:13 -0300

    Programa da aula sobre importações

---
+++ exercicios/programa01/src/fundamentos/Import.java
@@ -0,0 +1,19 @@
+package fundamentos;
+
+import java.util.Date;
+
+import static java.lang.System.*;
+
+public class Import {
+
+    public static void main(String[] args) {
+
+        java.lang.String s = "Bom dia!";
+        out.println(s);
+
+        Date d = new Date();
+        out.println(d);
+
+    }
+
+}
