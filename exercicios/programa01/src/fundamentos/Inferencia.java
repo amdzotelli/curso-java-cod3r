849be6204ca6d0cb3b698249b0b586ff9d281efe
Amanda Zotelli <amanda.zotelli@mercadolivre.com>
2022-10-13 19:35:18 -0300

    Cria programa testando a Inferência de Tipos em Java

---
+++ exercicios/programa01/src/fundamentos/Inferencia.java
@@ -0,0 +1,23 @@
+package fundamentos;
+
+import static java.lang.System.*;
+
+public class Inferencia {
+
+    public static void main(String[] args) {
+
+        double a =  4.5;
+        out.println(a);
+
+        var b = 4.5;
+        out.println(b);
+
+        var c = "Texto";
+        out.println(c);
+
+        a = 7;
+        out.println(a);
+
+    }
+
+}
