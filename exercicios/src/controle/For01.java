cb29ed1f01590c7c49c5fbab14d28c21aeca46ec
Amanda Zotelli <amanda.zotelli@mercadolivre.com>
2022-10-20 11:35:36 -0300

    Primeiro programa com o exemplo da variável de controle for no curso

---
+++ exercicios/src/controle/For01.java
@@ -0,0 +1,15 @@
+package controle;
+
+import static java.lang.System.*;
+
+public class For01 {
+
+    public static void main(String[] args) {
+
+        for (int contador = 20; contador >= 0; contador -= 2 ) {
+            out.printf("i = %d %n", contador);
+        }
+
+    }
+
+}
