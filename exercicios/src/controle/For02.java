be1ea5dbfdd70490d55a7a21505a1dedbde05342
Amanda Zotelli <amanda.zotelli@mercadolivre.com>
2022-10-25 19:50:32 -0300

    Cria programa da aula de For 02

---
+++ exercicios/src/controle/For02.java
@@ -0,0 +1,14 @@
+package controle;
+
+import static java.lang.System.out;
+
+public class For02 {
+
+    public static void main(String[] args) {
+        for (int contador = 0; contador <= 60; contador += 4 ) {
+            out.printf("i = %d %n", contador);
+        }
+
+    }
+
+}
